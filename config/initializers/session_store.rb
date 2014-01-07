92804c495d8666abde84361e06e6a1ff25a24ec5
Al Snow <jasnow@hotmail.com>
2014-01-07 10:14:32 -0500

    More updating to Rails 4.1.x

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-# RAILS 4.0: SampleApp31::Application.config.session_store :upgrade_signature_to_encryption_cookie_store, key: '_sample_app_3_1_session'
-# Rails 3.2.x:
-SampleApp31::Application.config.session_store :cookie_store, key: '_sample_app_3_1_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# SampleApp31::Application.config.session_store :active_record_store
+Rails.application.config.session_store :cookie_store, key: '_sample_app31_session'
