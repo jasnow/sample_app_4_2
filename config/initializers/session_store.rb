de9ec20caeead5d59aa0f0db04dc0299476da551
Al Snow <jasnow@hotmail.com>
2014-04-14 08:55:58 -0400

    Fixed cookie issue

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_sample_app31_session'
+Application.config.session_store :active_record_store
+
